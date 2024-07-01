40e5bb4a4fd95c9fe94ba554abd810eaef5aa110
klusvarghi <71743008+KLusvarghi@users.noreply.github.com>
2024-07-01 13:11:04 -0300

    banner: implementação do banner

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import styled from "styled-components";
 import EstilosGlobais from "./components/EstilogsGlobais";
 import Cabecalho from "./components/Cabecalho";
 import BarraLateral from "./components/BarraLateral";
+import Banner from "./components/Banner";
+import backgroundImage from "./assets/banner.png"
 
 // criando um component styled
   // definindo como contante - dando o nome - colocando o "styled" da importação - a tag HTMl que queremos interpretar - crase ``
@@ -16,6 +18,7 @@ function App() {
     <EstilosGlobais/>
     <Cabecalho/>
     <BarraLateral/>
+    <Banner txt="A galeria mais completa de fotos do espaço!" backgroundImage={backgroundImage}/>
   </FundoGradiente>
 }
 
--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,12 +1,29 @@
 import styled from 'styled-components';
 
-const BannerEstilziado = styled.figure``;
+const BannerEstilziado = styled.figure`
+  background-image: ${prop => `url(${prop.$backgroundImage})`};
+  background-repeat: no-repeat;
+  flex-grow: 1;
+  min-height: 328px;
+  max-width: 100%;
+  display: flex;
+  align-items: center;
+  margin: 0;
+  border-radius: 20px;
+`;
 
-const TituloEstilizado = styled.h1``;
+const TituloEstilizado = styled.h1`
+  font-weight: 400;
+  font-size: 40px;
+  line-height: 48px;
+  color: #FFF;
+  max-width: 300px;
+  padding-left: 0 64px;
+`;
 
 export const Banner = ({ txt, backgroundImage }) => {
   return (
-    <BannerEstilziado >
+    <BannerEstilziado $backgroundImage={backgroundImage}>
       <TituloEstilizado>{txt}</TituloEstilizado>
     </BannerEstilziado>
   );
